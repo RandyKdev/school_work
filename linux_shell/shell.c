90a534cdedab98d10dd653dae029fb26c9f5b1b8
Randy Kwalar <rkwalar@gmail.com>
2022-01-01 22:09:31 +0100

    Add history feature in linux shell

--- linux_shell/shell.c
+++ linux_shell/shell.c
@@ -21,10 +21,23 @@ void get_next_argument(char *input, char *args[], char argc) {
     }
 }
 
+void reset_arr(char *arr[]) {
+    int i = 0;
+    while(arr[i] != NULL && i < MAX_ARGS) {
+        free(arr[i]);
+        arr[i] = NULL;
+        i++;
+    }
+}
+
 void split_user_input(char *input, char *args[]) {
     char *buffer;
     int argc = 0;
 
+    // reset args array, frees all used memory locations
+    reset_arr(args);
+
+    // fills args withs args from user
     get_next_argument(input, args, argc);
 
     while (args[argc] != NULL && argc < MAX_ARGS) {
@@ -46,33 +59,93 @@ void read_user_input(char *args[]) {
     split_user_input(input, args);
 }
 
-bool ends_with_ampersand(char *args[]) {
+int ends_with_ampersand(char *args[]) {
     int i;
     for (i = 0; args[i] != NULL && i < MAX_ARGS; i++);
 
     if (strcmp(args[--i], "&") == 0) {
-        return true;
+        return i;
     }
 
-    return false;
+    return 0;
 }
 
-void execute_command_in_child_process(char *args[]) {
-    pid_t child_state = fork();
+bool is_history_command(char *args[]) {
+    return strcmp(args[0], "!") == 0 && strcmp(args[1], "!") == 0 && args[2] == NULL;
+}
 
+void print_arr(char *arr[]) {
+    printf("osh> ");
+    int i  = 0;
+    while(arr[i] != NULL && i < MAX_ARGS) {
+        printf("%s ", arr[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+void copy_arr(char *dest[], char *src[]) {
+    int i = 0;
+    while(src[i] != NULL && i < MAX_ARGS) {
+        dest[i] = (char *) malloc((strlen(src[i]) + 1) * sizeof(char));
+        strcpy(dest[i], src[i]);
+        i++;
+    }
+}
+
+void prepare_history_command(char *args[], char *history[]) {
+    // prints previous command
+    print_arr(history);
+
+    // frees "!" in args
+    free(args[0]);
+    free(args[1]);
+    args[0] = NULL;
+    args[1] = NULL;
+            
+    // copies command in history to args
+    copy_arr(args, history);
+}
+
+void execute_command_in_child_process(char *args[], char *history[]) {
+    if(is_history_command(args)) {
+        puts("history command");
+        prepare_history_command(args, history);
+    }
+
+    int ampersand_location = ends_with_ampersand(args);
+    if(ampersand_location) {
+        puts("ampersand found");
+        free(args[ampersand_location]);
+        args[ampersand_location] = NULL;
+    }
+
+    pid_t child_state = fork();
+    
     if (child_state == 0) {
         execvp(args[0], args);
     } else if (child_state == -1) {
         printf("Child process couldn't be created\n");
     } else {
-        if (!ends_with_ampersand(args)) {
+       
+        if (!ampersand_location) {
+             printf("%d\n", ampersand_location);
             wait(NULL);
+        }
+
+        reset_arr(history);
+        copy_arr(history, args);
+
+        if(ampersand_location) {
+            history[ampersand_location] = (char *) malloc(2 * sizeof(char));
+            strcpy(history[ampersand_location], "&");
         }
     }
 }
 
 int main(void) {
-    char *args[MAX_ARGS];
+    char *args[MAX_ARGS] = {NULL};
+    char *history[MAX_ARGS] = {NULL};
     int should_run = 1;
 
     while (should_run) {
@@ -81,9 +154,9 @@ int main(void) {
 
         read_user_input(args);
 
-        execute_command_in_child_process(args);
+        execute_command_in_child_process(args, history);
 
-        should_run = 0;
+        // should_run = 0;
     }
 
     return 0;
